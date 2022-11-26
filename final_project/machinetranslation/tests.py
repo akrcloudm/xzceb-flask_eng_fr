fbac96131e2953cfbf91efcc91cde9953b32ca6f
akrcloudm <112798071+akrcloudm@users.noreply.github.com>
2022-11-26 16:46:19 +0530

    Create tests.py

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,17 @@
+  def test1(self):
+        # Test Hello returns Bonjour
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        # Test Hello does not return Hello
+        self.assertNotEqual(english_to_french('Hello'), 'Hello')
+        # Test None returns empty string
+        self.assertNotEqual(english_to_french("None"), '')
+        # Test empty string returns empty string
+        self.assertNotEqual(english_to_french(0), 0)
+        
+  def test1(self):
+        self.assertEqual(french_to_english('Hello'), 'Bonjour')
+        self.assertNotEqual(french_to_english('Hello'), 'Hello')
+        self.assertNotEqual(french_to_english("None"), '')
+        self.assertNotEqual(french_to_english(0), 0)
+
+    
